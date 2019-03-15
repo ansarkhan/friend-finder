cbf27c9c7d2a7799d1cbdeb0d02dcfc965c04ec4
ansarkhan <ansarkhan@me.com>
2019-03-15 16:46:50 -0400

    home and survey skeleton complete

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+
+// creating the express server
+var app = express();
+
+var PORT = 8888;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes
+// require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
